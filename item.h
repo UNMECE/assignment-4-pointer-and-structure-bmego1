252b961065e14e37ea759aa363102ff090989bec
Bailey Mego <bmego1@triton.unm.edu>
2025-02-18 12:44:17 -0700

    finished

---
+++ item.h
@@ -0,0 +1,65 @@
+include <stdio.h>
+
+
+size=5
+Item *item=(Item*)malloc(size*size of item);
+
+void add_item(Item *item_list, double price, char *sku, char *category, char *name, int index);
+	
+
+	double price
+		*item_list=price 
+		price=("%d");
+		  
+
+	char *sku
+		*item_list=*sku
+		*sku=("%d");
+
+	char *category
+		*item_list=*category
+		*category=&time_of_day
+		if(*item_list="cereal", "cereal"= breakfast);
+			else(*item_list!=
+
+
+
+	char *name
+		*item_list=*name
+		printf("%d", *name)
+
+	int index
+		*item_list=index
+			printf(" \n\n"
+
+
+void free_items(Item *item_list, int size);
+
+
+
+double average_price(Item *item_list, int size);
+	size=5;
+
+	Item *item= (Item*) malloc(size*size_of_item);
+		malloc/5=average_price;
+
+
+void print_items(Item *item_list, int size);
+	for(int i=0; i<=2; i++){
+		printf("Item name = \n\n", add_item *char name);
+		printf("sku =  \n\n", add_item *sku);
+		printf("item category = \n\n", add_item *category);
+		printf("item price = \n\n", add_item *price);
+	}
+
+
+
+
+int main() {
+
+		
+
+
+
+return 0;
+}
