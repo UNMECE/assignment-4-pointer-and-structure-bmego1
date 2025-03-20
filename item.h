0dbb83858a580594967678758dd068eaccf6f02f
Bailey Mego <bmego1@vesta.unm.edu>
2025-03-20 11:20:05 -0600

    finished

--- item.h
+++ item.h
@@ -1,65 +1,22 @@
-include <stdio.h>
+#ifndef PIXEL_H
+#define PIXEL_H
+
+struct _Item {
+	float price;
+	char *sku;
+	char *name;
+	char *category;
+};
 
 
-size=5
-Item *item=(Item*)malloc(size*size of item);
-
-void add_item(Item *item_list, double price, char *sku, char *category, char *name, int index);
-	
-
-	double price
-		*item_list=price 
-		price=("%d");
-		  
-
-	char *sku
-		*item_list=*sku
-		*sku=("%d");
-
-	char *category
-		*item_list=*category
-		*category=&time_of_day
-		if(*item_list="cereal", "cereal"= breakfast);
-			else(*item_list!=
+typedef struct _Item Item;
 
 
-
-	char *name
-		*item_list=*name
-		printf("%d", *name)
-
-	int index
-		*item_list=index
-			printf(" \n\n"
-
-
+void add_item(Item *item_list, float price, char *sku, char *name, char *category, int index);
 void free_items(Item *item_list, int size);
-
-
-
-double average_price(Item *item_list, int size);
-	size=5;
-
-	Item *item= (Item*) malloc(size*size_of_item);
-		malloc/5=average_price;
-
-
+void average_price(Item *item_list, int size);
 void print_items(Item *item_list, int size);
-	for(int i=0; i<=2; i++){
-		printf("Item name = \n\n", add_item *char name);
-		printf("sku =  \n\n", add_item *sku);
-		printf("item category = \n\n", add_item *category);
-		printf("item price = \n\n", add_item *price);
-	}
+
+#endif
 
 
-
-
-int main() {
-
-		
-
-
-
-return 0;
-}
